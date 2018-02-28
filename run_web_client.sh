79d86273ad7c74101bdfb59f637d9e1a0410d43d
koplyarov <koplyarov.da@gmail.com>
2018-02-28 22:17:49 +0300

    Use core component in web-client

--- run_web_client.sh
+++ run_web_client.sh
@@ -1,2 +1,7 @@
-cd web_client
+SCRIPT_DIR="$(dirname $(readlink -f $0))"
+ROOT_DIR="$SCRIPT_DIR"
+
+export PYTHONPATH="$ROOT_DIR/build/bin"
+
+cd "$ROOT_DIR/build/bin/jmr_web_client"
 FLASK_APP=web_client.py flask run
--- web_client/web_client.py
+++ web_client/web_client.py
@@ -1,13 +1,24 @@
 import os
+import pyjoint_loader
+
+from joint_adapters import *
 
 from flask import Flask
-app = Flask(__name__)
-
 from flask import render_template, request
+
+
+app = Flask(__name__)
+core = pyjoint_loader.LoadModule('../core/Core.jm')
+client_session = core.GetRootObject(jmr_IClientSession, 'MakeClientSession')
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        version=client_session.GetVersionString()
+    )
+
 
 @app.route('/navigation')
 @app.route('/navigation/')
@@ -15,6 +26,7 @@ def navigation():
     path = request.args.get('path', '/')
     return render_template(
         'navigation.html',
+        version=client_session.GetVersionString(),
         path=path,
         dirs=['dir1', 'dir2', 'dir3'],
         path_join=os.path.join,
